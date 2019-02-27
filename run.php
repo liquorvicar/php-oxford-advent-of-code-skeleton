6203589c0ae81f0b54a34fdbfc0f0ea071b32662
Andy Clyde <a.clyde@elsevier.com>
2019-02-27 13:46:21 +0000

    Add composer scripts and PHPUnit config

--- run.php
+++ run.php
@@ -11,11 +11,11 @@ $input = file_get_contents($inputFilename);
 
 $answer = new PHPOxford\Answer();
 
-$options = getopt('p');
+$options = getopt('p:');
 
-if (!isset($options['p']) || $options['p'] === 'one') {
-    echo $answer->one($input);
-} elseif ($options['p'] === 'two') {
+if (isset($options['p']) && $options['p'] === 'two') {
     echo $answer->two($input);
+} else {
+    echo $answer->one($input);
 }
 echo "\n";
